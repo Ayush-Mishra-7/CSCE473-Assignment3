6754f381456ea1e98aea16ed6445a361b21050c6
Sky Martin <martinskylor@gmail.com>
2020-10-01 21:22:40 -0500

    Add files via upload
    Corrected Operators

--- sobel.m
+++ sobel.m
@@ -1,26 +1,29 @@
 function output = sobel(image, threshold)
-%SOBEL Summary of this function goes here
-%  image must be in grayscale
+% image must be in grayscale
+% the two parameters are your input image, and the threshold (0,255)
 close all;
 
+% Convert image to double and prep output
 image = im2double(image);
 output = image;
+orientation = image;
 
 % Ready the X and Y Masks
-mask_X = [-1 -2 -1; 0 0 0; 1 2 1];
-mask_Y = [1 0 -1; 2 0 -2; 1 0 -1];
+mask_X = [1 0 -1; 2 0 -2; 1 0 -1];
+mask_Y = [-1 -2 -1; 0 0 0; 1 2 1];
 
 for i = 1: size(image, 1) - 2
     for j = 1: size(image, 2) - 2
         
-        grad_X = sum(sum(mask_X*image(i:i + 2, j:j + 2)));
+        % Make Gradients
+        grad_X = sum(sum(mask_X.*image(i:i + 2, j:j + 2)));
         grad_Y = sum(sum(mask_Y.*image(i:i + 2, j:j + 2))); 
         
         % Edge Magnitude
         output(i + 1, j + 1) = sqrt(grad_X.^2 + grad_Y.^2);
         
         % Edge Orientation
-%         output(i + 1, j + 1) = atan(grad_X ./ grad_Y);
+        orientation(i + 1,j + 1) = atan(grad_X ./ grad_Y);
         
     end
 end
@@ -29,11 +32,11 @@ output = output > threshold/255;
 
 figure;
 imshow(output);
-title('My Sobel');
-figure
-imshow(edge(image));
-title('Edge()');
+title('Sobel');
 
-imshow(output);
+figure
+imshow(orientation);
+title('Edge Orientation');
+
 end
 
