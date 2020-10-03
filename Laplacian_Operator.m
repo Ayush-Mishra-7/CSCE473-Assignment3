d88959f2c6bf25f2a4a81c975702c1a8e706fb5a
Ayush Mishra <dayushmishra@gmail.com>
2020-10-02 19:11:52 -0500

    Add files via upload

--- Laplacian_Operator.m
+++ Laplacian_Operator.m
@@ -1,21 +1,18 @@
-function output_image = Laplacian_Operator()
+function output_image = Laplacian_Operator(image,thres)
 
 %First lets convert the image to double
-image = imread('Images/175032.jpg')
 figure(1);
 imshow(image);
-image = imgaussfilt(image,0.5);
-figure(2);
-imshow(image);
+no_color_channels = size(image,3);
+if no_color_channels > 1
+    image = rgb2gray(image);
+end
+
 image = im2double(image);
+
 
 lap_filter = [0 1 0; 1 -4 1; 0 1 0];
 
-
-%output_image = conv2(image,lap_filter);
-%output_image = edge(image, 'log', 0.2);
-%figure(1);
-%imshow(output_image);
 
 f_img = zeros(size(image));
 
@@ -24,20 +21,21 @@ for i = 1:size(image,1) - 2
         
         pixel_grad = sum(sum(lap_filter.*image(i:i+2, j:j+2)));
        
-        f_img(i,j) = pixel_grad;
+        f_img(i+1,j+1) = pixel_grad;
     end
 end
 
-figure(3);
+figure(2);
 imshow(f_img);
 
-thres = 0.05;
 
-f_img(f_img < thres) = 0;
-f_img(f_img >= thres) = 1;
+%f_img(f_img < (thres/255)) = 0;
+%f_img(f_img >= (thres/255)) = 1;
+
+f_img = f_img > (thres/255);
 
 output_image = f_img;
-figure(4);
+figure(3);
 imshow(output_image);
 
 %output_image = im2bw(output_image);
--- Marr_Hildreth.m
+++ Marr_Hildreth.m
@@ -1,55 +1,67 @@
-function output_image = Marr_Hildreth()
+function output_image = Marr_Hildreth(img,sig)
 %Steps
-%filter the image with gaussian
+%filter the img with gaussian
 %Compute the second derivative
-%Threshold the image, if > 0 -> white, else <0 black
+%Threshold the img, if > 0 -> white, else <0 black
 
 %Find the zero crossings
 %Transistion from black to white
 
-image = imread('Images/3096.jpg');
 
-no_color_channels = size(image,3);
+no_color_channels = size(img,3);
 
 if no_color_channels > 1
-    image = rgb2gray(image);
+    img = rgb2gray(img);
 end
     
-image = im2double(image);
+img = im2double(img);
     
-sig = 0.5;
 filter_size = (ceil(sig*3))*2 + 1;
 
 %Creating the filter
-image_filter = fspecial('gaussian', filter_size); 
+%Log filter does the gaussian blur and at the same time calculates the
+%Second derivative
+img_filter = fspecial('log', filter_size, sig); 
 
-%convolving the image with the filter
-output_image = conv2(image,image_filter);
+%convolving the img with the filter
+output_image = conv2(img,img_filter);
 figure(1);
 imshow(output_image);
 
 %%Computing the second derivate
-[image_gradientX, image_gradientY] = gradient(output_image);
+%[img_gradientX, img_gradientY] = gradient(output_img);
 
-[img_grad_secondXX, img_grad_secondXY] = gradient(image_gradientX);
+%[img_grad_secondXX, img_grad_secondXY] = gradient(img_gradientX);
 
-[img_grad_secondYX, img_grad_secondYY] = gradient(image_gradientY);
+%[img_grad_secondYX, img_grad_secondYY] = gradient(img_gradientY);
 
-second_derivative = img_grad_secondXX + img_grad_secondYY + img_grad_secondXY + img_grad_secondYX;
+%second_derivative = img_grad_secondXX + img_grad_secondYY + img_grad_secondXY + img_grad_secondYX;
 
 f_img = zeros(size(output_image));
 
-for i = 1:size(output_image,1) - 2
-    for j = 1:size(output_image,2) - 2
-        
-        if second_derivative(i,j) > 0 && second_derivative(i,j+1) <0
-            f_img(i,j) = 1;
+%Checking for zero crossing
+%loop through the points and check if the signs change in x-direction or
+% if the sign changes in the y-direction
+% Also checking if the pixel crosses zero crossing diagonally
+
+for i = 2:size(output_image,1) - 2
+    for j = 2:size(output_image,2) - 2
+        if (output_image(i,j+1)>=0 && output_image(i,j-1)<0) || (output_image(i,j+1)<0 && output_image(i,j-1)>=0)
+            f_img(i,j)= output_image(i,j+1);
+        elseif (output_image(i+1,j)>=0 && output_image(i-1,j)<0) || (output_image(i+1,j)<0 && output_image(i-1,j)>=0)
+            f_img(i,j)= output_image(i,j+1);
+        elseif (output_image(i+1,j+1)>=0 && output_image(i-1,j-1)<0) || (output_image(i+1,j+1)<0 && output_image(i-1,j-1)>=0)
+            f_img(i,j)= output_image(i,j+1);
+        elseif (output_image(i-1,j+1)>=0 && output_image(i+1,j-1)<0) || (output_image(i-1,j+1)<0 && output_image(i+1,j-1)>=0)
+            f_img(i,j)=output_image(i,j+1);
         end
-        
     end
 end
 
+
+
 figure(2);
 imshow(f_img);
+
 
 end
